647301cd0e5c044479f754613a7cbe5b39c272c9
yassine <rezguiyassine224@gmail.com>
2025-01-24 18:59:00 +0100

    fix the validator

--- gestiondestock/src/main/java/com/yassinecoding/gestiondestock/validator/CategoryValidator.java
+++ gestiondestock/src/main/java/com/yassinecoding/gestiondestock/validator/CategoryValidator.java
@@ -11,7 +11,7 @@ public class CategoryValidator {
     public static List<String> validate(CategoryDto categoryDto) {
         List<String> errors = new ArrayList<>();
         // Check if the category designation is empty or rturn error message
-        if(categoryDto == null   ||StringUtils.hasLength(categoryDto.getCode())) {
+        if(categoryDto == null   || !StringUtils.hasLength(categoryDto.getCode())) {
             errors.add("Veuillez renseigner le code de la catégorie");
         }
         return errors;
--- gestiondestock/src/main/java/com/yassinecoding/gestiondestock/validator/UtilisateurValidateur.java
+++ gestiondestock/src/main/java/com/yassinecoding/gestiondestock/validator/UtilisateurValidateur.java
@@ -13,44 +13,55 @@ public class UtilisateurValidateur {
     public static List<String> validate(UtilisateurDto utilisateurDto) {
 
         List<String> errors = new ArrayList<>();
+
+        if(utilisateurDto == null){
+            errors.add("Veuillez renseigner le nom de l'utilisateur");
+            errors.add("Veuillez renseigner le prenom de l'utilisateur");
+            errors.add("Veuillez renseigner l'email de l'utilisateur");
+            errors.add("Veuillez renseigner le mot de passe de l'utilisateur");
+            errors.add("Veuillez renseigner la date de naissance de l'utilisateur");
+            errors.add("Veuillez renseigner l'adresse de l'utilisateur");
+            return errors;
+        }
+
+
         // Check if the user name is empty or return error message
-        if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getNom()) ) {
+        if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getNom()) ) {
             errors.add("Veuillez renseigner le nom de l'utilisateur");
         }
         // Check if the user first name is empty or return error message
-        if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getPrenom()) ) {
+        if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getPrenom()) ) {
             errors.add("Veuillez renseigner le prenom de l'utilisateur");
         }
         // Check if the user email is empty or return error message
-        if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getEmail()) ) {
+        if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getEmail()) ) {
             errors.add("Veuillez renseigner l'email de l'utilisateur");
         }
-        // Check if the user date of birth is empty or return error message
-        if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getDateDeNaissance().toString()) ) {
-            errors.add("Veuillez renseigner la date de naissance de l'utilisateur");
-        }
+
         // Check if the user password is empty or return error message
-        if(utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getMotDePasse())){
+        if(utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getMotDePasse())){
             errors.add("Veuillez renseigner le mot de passe de l'utilisateur");
+        }
+        if(utilisateurDto.getDateDeNaissance() == null){
+            errors.add("Veuillez renseigner la date de naissance de l'utilisateur");
         }
         // Check if the user address is empty or return error message
         if(utilisateurDto == null ){
             errors.add("Veuillez renseigner l'adresse de l'utilisateur");
         }else {
-            // Check if the user photo is empty or return error message
-            if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getAdresse().getAdresse2())) {
+            if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getAdresse().getAdresse2())) {
                 errors.add("Le champ adresse 1 est obligatoire");
             }
-            if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getAdresse().getVille())) {
+            if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getAdresse().getVille())) {
                 errors.add("Le champ adresse 2 est obligatoire");
             }
-            if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getAdresse().getAdresse1())) {
+            if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getAdresse().getAdresse1())) {
                 errors.add("Le champ ville  est obligatoire");
             }
-            if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getAdresse().getCodePostale())) {
+            if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getAdresse().getCodePostale())) {
                 errors.add("Le champ Code Postal   est obligatoire");
             }
-            if (utilisateurDto == null || StringUtils.hasLength(utilisateurDto.getAdresse().getPays())) {
+            if (utilisateurDto == null || !StringUtils.hasLength(utilisateurDto.getAdresse().getPays())) {
                 errors.add("Le champPays   est obligatoire");
             }
 
@@ -60,4 +71,6 @@ public class UtilisateurValidateur {
 
         return errors;
     }
+    // Todo : add validato for date
+
 }
