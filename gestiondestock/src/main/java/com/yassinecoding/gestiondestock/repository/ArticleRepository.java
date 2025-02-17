e378079909734fca320a00f60426a09c13f05e66
yassine <rezguiyassine224@gmail.com>
2025-02-17 22:28:42 +0100

    Added exception handling for EntityNotFoundException and InvalidEntityException in RestExceptionHandler

--- gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/ArticleRepository.java
+++ gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.yassinecoding.gestiondestock.repository;
 import com.yassinecoding.gestiondestock.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +12,15 @@ public interface ArticleRepository extends JpaRepository<Article, Integer> {
     Optional<Article> findArticleByCodeArticle(String codeArticle);
     // ecrire une requete personalisee
 
+//    @Query("select a from Article a where a.codeArticle = :code and a.designation = :designation")
+//    List<Article> findByCustomerQuery(@Param("code") String code, @Param("designation") String designation);
+//
+//    // i write native query for the same query
+//    @Query(value = "select * from Article a where a.codeArticle = :code ", nativeQuery = true)
+//    List<Article> findByCustomerQueryNative(@Param("code") String code);
+//
+//    //
+//    List<Article> findByCodeArticleIgnoreCaseAAndDesignation(String codeArticle,String designation);
 
 
-
-    @Query("select a from Article a where a.designation = :designation")
-    List<Article> findByDesignation(String designation);
-
-    @Query("select a from Article a where a.category.id = :categoryId")
-    List<Article> findByCategoryId(Integer categoryId);
 }
