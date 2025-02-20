95807f370531ef406e4c3d676a10b5dca5463fa6
Rezgui Yassine <rezguiyassine224@gmail.com>
2025-02-20 20:48:17 +0100

    Fournisseur services and controller work 👍

--- gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/EntrepriseReposittory.java
+++ gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/EntrepriseReposittory.java
@@ -3,5 +3,11 @@ package com.yassinecoding.gestiondestock.repository;
 import com.yassinecoding.gestiondestock.model.Entreprise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface EntrepriseReposittory extends JpaRepository< Entreprise , Integer > {
+    Optional<Entreprise>findById(Integer id);
+    Optional<Entreprise>findByNom(String nom);
+    Optional<Entreprise> findByCodeFiscal(String codeFiscal);
 }
--- gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/FournisseurRepository.java
+++ gestiondestock/src/main/java/com/yassinecoding/gestiondestock/repository/FournisseurRepository.java
@@ -1,7 +1,14 @@
 package com.yassinecoding.gestiondestock.repository;
 
+import com.yassinecoding.gestiondestock.dto.FournisseurDto;
 import com.yassinecoding.gestiondestock.model.Fournisseur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Integer> {
+    Optional<Fournisseur> findFournisseurById(Integer id);
+    Optional<Fournisseur> findFournisseurByNom(String nom);
+
+
 }
